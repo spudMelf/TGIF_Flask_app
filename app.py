f33c757d52c0c813bcf5c5dcb0673219f2b82a15
spudMelf <hurleyeamon58@gmail.com>
2022-11-30 19:26:22 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import tensorflow as tf
+import math
+
+# Create flask app
+flask_app = Flask(__name__, static_folder='staticFiles')
+model = pickle.load(open("model.pkl", "rb"))
+tf_model = tf.keras.models.load_model('akin/model')
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [str(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = tf_model.predict(features)
+    
+    
+
+    # custom function
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+
+    # define vectorized sigmoid
+    sigmoid_v = np.vectorize(sigmoid)
+
+    y_new = sigmoid_v(np.array([prediction[0]]))
+
+    y_new = y_new * 100
+
+    y_new_text = '{:.0f}%'.format(int(y_new[0]))
+
+    return render_template("index.html", prediction_text = "The model computes that the bill will pass with likelihood:  {}".format(y_new_text))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
